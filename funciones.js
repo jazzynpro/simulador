05ede1a17e854bc1389c38bf019af0db922018d4
jazzynpro <reader_jaz@hotmail.com>
2026-04-15 21:33:04 -0500

    Hasta paso 4

--- funciones.js
+++ funciones.js
@@ -1,1 +1,19 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos){
+    let disponible = ingresos - egresos;
+    if(disponible < 0){
+        disponible = 0;
+    }
+    return disponible;
+}
+ 
+function calcularCapacidadDePago(montoDisponible){
+    let capacidadDePago = montoDisponible / 2;
+ 
+    return capacidadDePago;
+}
+ 
+function texto (id,vari){
+    let texto1 = document.getElementById(id);
+    texto1.innerText=vari.toFixed(2);    
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+ 
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+ 
+    let disponible = calcularDisponible(ingresos, egresos);
+ 
+    texto("spnDisponible", disponible);
+ 
+    let capacidadDePago = calcularCapacidadDePago(disponible);
+ 
+    texto("spnCapacidadPago", capacidadDePago);
+}
+
